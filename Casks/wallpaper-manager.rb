17dfbdbbc801c8ab89d9e78f1c898f19f2ed745e
Kimimichi SHIOIRI <127085707+mimifuwacc@users.noreply.github.com>
2026-06-26 20:18:35 +0900

    Add wallpaper-manager cask 0.2.0

---
+++ Casks/wallpaper-manager.rb
@@ -0,0 +1,20 @@
+cask "wallpaper-manager" do
+  version "0.2.0"
+  sha256 "f60ca5d9287e8b895b024fa0968273fcab34e30c1af2c03c2c7b573e51287b6d"
+
+  url "https://github.com/mimifuwacc/macos-wallpaper-manager/releases/download/v#{version}/WallpaperManager-v#{version}.dmg"
+  name "Wallpaper Manager"
+  desc "Switches the desktop wallpaper based on each display's orientation"
+  homepage "https://github.com/mimifuwacc/macos-wallpaper-manager"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :tahoe"
+
+  app "WallpaperManager.app"
+
+  zap trash: "~/Library/Preferences/cc.mimifuwa.WallpaperManager.plist"
+end
